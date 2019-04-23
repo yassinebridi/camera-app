3fb9974ea4dbbaead7a80bcb1686f56afd2f9abf
Yassine Bridi <ybridi@gmail.com>
2019-04-23 16:31:39 +0100

    Updates

---
+++ precache-manifest.10aab42927d147bd9f3aa98bb9b0286d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8f29bb266497511826aa20a707a0a6",
+    "url": "/camera-app/index.html"
+  },
+  {
+    "revision": "46d51831a78aca325a43",
+    "url": "/camera-app/static/css/main.1492d546.chunk.css"
+  },
+  {
+    "revision": "7e7b8e1de1f6435143f5",
+    "url": "/camera-app/static/js/2.6b34cb44.chunk.js"
+  },
+  {
+    "revision": "46d51831a78aca325a43",
+    "url": "/camera-app/static/js/main.1083e33a.chunk.js"
+  },
+  {
+    "revision": "3eae3818fee16507d3ee",
+    "url": "/camera-app/static/js/runtime~main.0cd7a794.js"
+  }
+]);
